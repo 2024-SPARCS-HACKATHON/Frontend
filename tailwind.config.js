907a0c9a3d6b87a84b2d097ec40a17bc1f1518aa
choihooo <hochoi8621@naver.com>
2024-10-19 17:44:09 +0900

    폰트 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        phudu: ["Phudu", "sans-serif"],
+        courier: ["Courier New", "monospace"],
+        noto: ["Noto Sans KR", "sans-serif"],
+        climate: ["Climate Crisis", "cursive"],
+      },
+    },
   },
   plugins: [],
 };
